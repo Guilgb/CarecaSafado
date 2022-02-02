817641d489107538a1041999f198364b506245ff
Guilherme Bezerra <guilherme.gb@hotmail.com>
2022-02-02 10:58:46 -0300

    iniciando conjunto

---
+++ Estudando/Collections/conjunto.py
@@ -0,0 +1,7 @@
+usuarios_data_science = [15, 23, 45, 65]
+usuarios_machine_learning = [12, 23, 43, 2]
+
+
+assistiram = usuarios_data_science.copy()
+assistiram.extend(usuarios_machine_learning)
+print(assistiram)
