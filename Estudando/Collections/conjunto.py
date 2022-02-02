6e3dec0a5aa09a4198411b02e1fbbf1c1ca86a7e
Guilherme Bezerra <guilherme.gb@hotmail.com>
2022-02-02 11:10:19 -0300

    nada

--- Estudando/Collections/conjunto.py
+++ Estudando/Collections/conjunto.py
@@ -4,4 +4,5 @@ usuarios_machine_learning = [12, 23, 43, 2]
 
 assistiram = usuarios_data_science.copy()
 assistiram.extend(usuarios_machine_learning)
-print(assistiram)
+# Criando conjunto com set(), e não existe posição
+print(set(assistiram), "\nTamanho:", len(set(assistiram)))
