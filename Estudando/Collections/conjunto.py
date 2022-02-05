c9ee03f2b433a0871f6b9efba9d845ac2308ef02
Guilherme Bezerra <guilherme.gb@hotmail.com>
2022-02-05 19:28:59 -0300

    nada

--- Estudando/Collections/conjunto.py
+++ Estudando/Collections/conjunto.py
@@ -6,3 +6,11 @@ assistiram = usuarios_data_science.copy()
 assistiram.extend(usuarios_machine_learning)
 # Criando conjunto com set(), e não existe posição
 print(set(assistiram), "\nTamanho:", len(set(assistiram)))
+# set elimina tudo que esta repetido
+
+nomes_proprios = {
+    "Guilherme": 12,
+    "Camyla": 13,
+    "Gael": 14
+}
+print(nomes_proprios["Guilherme"])
