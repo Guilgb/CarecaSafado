02e8bf7030bd78c725fc42718c519946f3493f7f
Guilherme Bezerra <guilherme.gb@hotmail.com>
2022-02-08 10:51:26 -0300

    last Telefone

--- Estudando/expressoesregulares/Telefone.py
+++ Estudando/expressoesregulares/Telefone.py
@@ -19,7 +19,9 @@ class Telefone:
     def fomat(self):
         padrao = "([0-9]{2})([0-9]{2})([0-9])([0-9]{4})([0-9]{4})"
         resposta = re.search(padrao, self.numero)
-        numero_formatado = f"+{resposta.group(1)} ({resposta.group(2)}) {resposta.group(3)} {resposta.group(4)}-{resposta.group(5)}"
+        numero_formatado = (f"+{resposta.group(1)} ({resposta.group(2)})"
+                            f"{resposta.group(3)}"
+                            f"{resposta.group(4)}-{resposta.group(5)}")
         return (numero_formatado)
 
     def __str__(self):
