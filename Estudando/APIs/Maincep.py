6aed202b8e1a13f04e77c2e8e9f9f391547ca508
Guilherme <guilherme.gb@hotmail.com>
2022-02-09 19:20:03 -0300

    print(cep)

--- Estudando/APIs/Maincep.py
+++ Estudando/APIs/Maincep.py
@@ -6,6 +6,7 @@ obj_cep = Acessocep(cep)
 cedro = obj_cep.acessar_url()
 ddd, cidade, uf = obj_cep.acessar_url()
 print(f"DDD: {ddd} \nCidade: {cidade} \nEstado: {uf}")
+print("CEP: ", obj_cep)
 
 # r = requests.get("https://viacep.com.br/ws/01001000/json/")
 # print(r.text)
