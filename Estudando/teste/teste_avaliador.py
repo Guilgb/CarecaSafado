5925ed4c4e67702491760615fd9f369e882d5b78
Guilherme Bezerra <guilherme.gb@hotmail.com>
2022-02-12 13:39:11 -0300

    Corrigindo teste

--- Estudando/teste/teste_avaliador.py
+++ Estudando/teste/teste_avaliador.py
@@ -3,19 +3,19 @@ import unittest
 from dominio import Usuario, Lance, Leilao, Avaliador
 
 
-def setUp(self):
-    self.gui = Usuario("Guilherme")
-    self.camyla = Usuario("Camyla")
-    self.gael = Usuario("Gael")
-
-    self.lance_gui = Lance(self.gui, 100)
-    self.lance_camy = Lance(self.camyla, 200)
-    self.lance_gael = Lance(self.gael, 20)
-
-    self.leilao = Leilao("Lance de Celulares")
-
-
 class TesteAvaliador(TestCase):
+
+    def setUp(self):
+        self.gui = Usuario("Guilherme")
+        self.camyla = Usuario("Camyla")
+        self.gael = Usuario("Gael")
+
+        self.lance_gui = Lance(self.gui, 100)
+        self.lance_camy = Lance(self.camyla, 200)
+        self.lance_gael = Lance(self.gael, 20)
+
+        self.leilao = Leilao("Lance de Celulares")
+
     def test_avalia(self):
 
         self.leilao.lances.append(self.lance_camy)
@@ -24,11 +24,12 @@ class TesteAvaliador(TestCase):
         avaliador = Avaliador()
         avaliador.avalia(self.leilao)
 
-        menor_valor_esperado = 100
+        menor_valor_esperado = 20
         self.assertEqual(menor_valor_esperado, avaliador.menor_lance)
 
         maior_valor_esperado = 200
         self.assertEqual(maior_valor_esperado, avaliador.maior_lance)
+
 
 if __name__ == '__main__':
     unittest.main()
